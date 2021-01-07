e3dc835ed0df368194a59c558c2a5f9db238044a
Matthew T. Gilbride <mgilbride@chariotsolutions.com>
2021-01-06 22:37:53 -0500

    start on random color and counts

--- service/src/game.rs
+++ service/src/game.rs
@@ -1,18 +1,27 @@
 use std::iter::Map;
 use rand::{seq::SliceRandom};
-use std::collections::HashSet;
+use std::collections::{HashSet, HashMap};
 use std::iter::FromIterator;
+use std::cell::Cell;
 
 const BOARD_SIZE: usize = 25;
 
+#[derive(Copy, Clone, PartialEq, Eq)]
 enum Team {
     Red,
     Blue
 }
 
+#[derive(Copy, Clone, PartialEq, Eq)]
+enum CardColor {
+    Team(Team),
+    Neutral,
+    Death
+}
+
 struct Card {
-    covered: bool,
-    team: Team,
+    covered: Cell<bool>,
+    color: CardColor,
     word: String
 }
 
@@ -26,7 +35,7 @@ struct TeamMember {
 }
 
 struct Game {
-    board: [[Card; 5]; 5],
+    board: [Card; 25],
     players: Map<Team, Vec<Player>>,
     turn: Team,
 }
@@ -39,4 +48,14 @@ fn generate_board_words(dictionary: HashSet<String>) -> Result<HashSet<String>, 
     let as_vector: Vec<String> = dictionary.into_iter().collect();
     let random_subset: Vec<String> = as_vector.choose_multiple(&mut rng, 25).cloned().collect();
     Ok(HashSet::from_iter(random_subset.iter().cloned()))
+}
+
+fn random_color(available_colors: HashSet<CardColor>) -> CardColor {
+    let mut rng = rand::thread_rng();
+    let as_vector: Vec<CardColor> = available_colors.into_iter().collect();
+    *as_vector.choose(&mut rng).unwrap_or(&CardColor::Neutral)
+}
+
+fn card_color_count(board: [Card; 25], color: CardColor) -> usize {
+    board.iter().filter(|card| card.color == color).count()
 }
