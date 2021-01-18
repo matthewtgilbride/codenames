b89cbb83742fdea7b41a3285d198267dd2a7a8e6
matthewtgilbride <matthewtgilbride@gmail.com>
2021-01-18 17:32:19 -0500

    build(initial): compilation with new wasmCloud api
    using `cargo build --target wasm32-wasi`

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -1,29 +1,26 @@
-// Copyright 2015-2020 Capital One Services, LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and 
-// limitations under the License.
+#[macro_use]
+extern crate serde_json;
 
-extern crate wascc_actor as actor;
+extern crate wapc_guest as guest;
+use actor_core as core;
+use actor_http_server as http;
+use actor_keyvalue as kv;
+use guest::prelude::*;
 
-use actor::prelude::*;
-
-actor_handlers! { codec::http::OP_HANDLE_REQUEST => hello_world, 
-                  codec::core::OP_HEALTH_REQUEST => health }
-
-fn hello_world(_payload: codec::http::Request) -> HandlerResult<codec::http::Response> {
-    Ok(codec::http::Response::ok())
+#[no_mangle]
+pub fn wapc_init() {
+    core::Handlers::register_health_request(health);
+    http::Handlers::register_handle_request(increment_counter);
 }
 
-fn health(_req: codec::core::HealthRequest
-) -> HandlerResult<()> {
-    Ok(())
+fn increment_counter(msg: http::Request) -> HandlerResult<http::Response> {
+    let key = msg.path.replace('/', ":");
+    let resp = kv::default().add(key, 1)?;
+
+    let result = json!({"counter": resp.value });
+    Ok(http::Response::json(&result, 200, "OK"))
+}
+
+fn health(_h: core::HealthCheckRequest) -> HandlerResult<core::HealthCheckResponse> {
+    Ok(core::HealthCheckResponse::healthy())
 }
