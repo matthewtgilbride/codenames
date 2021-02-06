49ca70b4c15547c3aec5c1e5851e31c7cf07bd9e
matthewtgilbride <matthewtgilbride@gmail.com>
2021-02-06 09:24:12 -0500

    feat(json): flatten CardColor serialization

--- service/domain/src/game/card/model.rs
+++ service/domain/src/game/card/model.rs
@@ -1,12 +1,25 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Serializer};
 
 use crate::game::model::Team;
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Deserialize)]
 pub enum CardColor {
     Team(Team),
     Neutral,
     Death,
+}
+
+impl Serialize for CardColor {
+    fn serialize<S>(&self, serializer: S) -> Result<<S as Serializer>::Ok, <S as Serializer>::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            CardColor::Team(t) => t.serialize(serializer),
+            CardColor::Neutral => serializer.serialize_str("Neutral"),
+            CardColor::Death => serializer.serialize_str("Death"),
+        }
+    }
 }
 
 pub const ALL_CARD_COLORS: [CardColor; 4] = [
