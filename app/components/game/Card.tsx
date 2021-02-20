961de77f0c735988cbd2b050fdf8a30542f65454
matthewtgilbride <matthewtgilbride@gmail.com>
2021-02-20 02:59:48 -0500

    feat(app): dynamic card font size

--- app/components/game/Card.tsx
+++ app/components/game/Card.tsx
@@ -25,31 +25,34 @@ export const Card: FC<CardProps> = ({
   player,
   card: { color, word },
   onClick,
-}) => (
-  <button
-    type="button"
-    onClick={onClick}
-    disabled={isDisabled(turn, color, player)}
-    css={`
-      background-color: ${color ? CardColorMap[color] : 'white'};
-      box-shadow: 0 0 2px 1px ${Palette.blue};
-      color: ${color === 'Death' ? neutral : contrast};
-      border-radius: 0.25rem;
-      padding: 1rem 0.1rem;
-      word-break: break-all;
-      font-size: 0.5rem;
-      ${beginAt(phoneLg)} {
-        font-size: 0.75rem;
-      }
-      ${beginAt(tabletPortrait)} {
-        font-size: unset;
-        padding: 2rem 0.1rem;
-      }
-    `}
-  >
-    {word}
-  </button>
-);
+}) => {
+  const size = word.length > 5 ? (1 / word.length) * 72 : 12;
+  return (
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={isDisabled(turn, color, player)}
+      css={`
+        background-color: ${color ? CardColorMap[color] : 'white'};
+        box-shadow: 0 0 2px 1px ${Palette.blue};
+        color: ${color === 'Death' ? neutral : contrast};
+        border-radius: 0.25rem;
+        padding: 0.75rem 0.1rem;
+        word-break: break-all;
+        font-size: ${size}px;
+        ${beginAt(phoneLg)} {
+          font-size: ${size * 1.5}px;
+        }
+        ${beginAt(tabletPortrait)} {
+          font-size: ${size * 2}px;
+          padding: 2rem 0.1rem;
+        }
+      `}
+    >
+      {word}
+    </button>
+  );
+};
 
 function isDisabled(turn: Team, color?: CardType, player?: Player): boolean {
   if (color) return true;
