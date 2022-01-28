2312fb01f7753dd4686d982e1706fbc08b156d7f
matthewtgilbride <matthewtgilbride@gmail.com>
2022-01-27 21:05:28 -0500

    feat(dynamo): start refactoring to dynamo

--- service/actix/src/game/dao.rs
+++ service/actix/src/game/dao.rs
@@ -1,8 +1,103 @@
+use aws_sdk_dynamodb::{
+    error::GetItemErrorKind::ResourceNotFoundException, model::AttributeValue, Client,
+    SdkError::ServiceError,
+};
+use chrono::Utc;
 use codenames_domain::{
     game::{dao::GameDao, model::GameData},
-    DaoError, DaoResult, Lowercase, StdResult,
+    DaoError,
+    DaoError::NotFound,
+    DaoResult, Lowercase, StdResult,
 };
 use redis::{Commands, Connection, Value};
+
+pub struct DynamoDao {
+    client: Client,
+}
+
+impl DynamoDao {
+    pub fn new() -> StdResult<DynamoDao> {
+        let shared_config = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(aws_config::load_from_env());
+        let client = Client::new(&shared_config);
+        Ok(DynamoDao { client })
+    }
+}
+
+impl Clone for DynamoDao {
+    fn clone(&self) -> Self {
+        DynamoDao::new().unwrap()
+    }
+}
+
+impl GameDao for DynamoDao {
+    fn get(&mut self, key: Lowercase) -> DaoResult<GameData> {
+        let request = &self
+            .client
+            .get_item()
+            .table_name("codenames")
+            .key("id", AttributeValue::S(key.value().to_string()));
+
+        let result = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(request.clone().send())
+            .map_err(|outer| match outer {
+                ServiceError { err, .. } => match &err.kind {
+                    ResourceNotFoundException(r) => DaoError::NotFound(r.to_string()),
+                    _ => DaoError::Unknown("wat".into()),
+                },
+                e => DaoError::Unknown(e.to_string()),
+            })?;
+
+        let item = result.item.ok_or(NotFound(key.value().to_string()))?;
+        let attribute = item.get("game").ok_or(DaoError::Unknown(
+            "could not find game attribute on dynamo result".into(),
+        ))?;
+        let game_string = attribute
+            .as_s()
+            .map_err(|_| DaoError::Unknown("could not get game as string".into()))?;
+
+        serde_json::from_str(game_string).map_err(|e| DaoError::Unknown(e.to_string()))
+    }
+
+    fn keys(&mut self) -> DaoResult<Vec<Lowercase>> {
+        todo!()
+        // let request = &self.client
+        //     .scan()
+        //     .table_name("codenames")
+        //     .attributes_to_get(vec!["id"]);
+        //
+        // let result = request.send().await?;
+        // let items = result.items.unwrap_or(Vec::new());
+        // let keys: Vec<String> = items.iter().map(|i| i.get("id")).collect();
+        // return Ok(keys.iter().map(|k| Lowercase::new(k)).collect())
+    }
+
+    fn set(&mut self, key: Lowercase, game: GameData) -> DaoResult<()> {
+        let request = &self
+            .client
+            .put_item()
+            .table_name("codenames")
+            .item("id", AttributeValue::S(key.value().to_string()))
+            .item("timestamp", AttributeValue::S(Utc::now().to_string()))
+            .item("game", AttributeValue::S(json!(game).to_string()));
+
+        match tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap()
+            .block_on(request.clone().send())
+        {
+            Ok(_) => Ok(()),
+            Err(e) => Err(DaoError::Unknown(e.to_string())),
+        }
+    }
+}
 
 pub struct RedisDao {
     con: Connection,
