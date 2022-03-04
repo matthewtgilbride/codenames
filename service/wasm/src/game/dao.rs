9514d6ab6f27233d85455d1819d48093c16d506b
matthewtgilbride <matthewtgilbride@gmail.com>
2022-03-04 13:34:54 -0500

    refactor(kv-dynamodb): wasmCloud lib upgrade - fixing compilation errors

--- service/wasm/src/game/dao.rs
+++ service/wasm/src/game/dao.rs
@@ -1,34 +1,44 @@
-use codenames_domain::game::dao::GameDao;
-use codenames_domain::game::model::Game;
-use codenames_domain::{DaoError, DaoResult, Lowercase};
 use async_trait::async_trait;
-use wasmbus_rpc::actor::prelude::WasmHost;
-use wasmbus_rpc::common::Context;
-
-use wasmcloud_interface_keyvalue::{IncrementRequest, KeyValue, KeyValueSender, ListAddRequest, SetRequest};
+use codenames_domain::{
+    game::{dao::GameDao, model::GameData},
+    DaoError, DaoResult, Lowercase,
+};
+use wasmbus_rpc::{actor::prelude::WasmHost, common::Context};
+use wasmcloud_interface_keyvalue::{
+    KeyValue, KeyValueSender, ListAddRequest, ListRangeRequest, SetRequest,
+};
 
 const ALL_GAMES_KEY: &str = "GAME_KEYS";
 
-#[derive(Clone)]
 pub struct WasmKeyValueDao {
     ctx: Box<Context>,
-    kv: KeyValueSender<WasmHost>
+    kv: KeyValueSender<WasmHost>,
+}
+
+impl Clone for WasmKeyValueDao {
+    fn clone(&self) -> Self {
+        return WasmKeyValueDao {
+            ctx: self.ctx.clone(),
+            kv: KeyValueSender::new(),
+        };
+    }
 }
 
 impl WasmKeyValueDao {
     pub fn new(ctx: &Context) -> Self {
         Self {
             ctx: Box::new(ctx.clone()),
-            kv: KeyValueSender::new()
+            kv: KeyValueSender::new(),
         }
     }
 }
 
 #[async_trait]
 impl GameDao for WasmKeyValueDao {
-    async fn get(&mut self, key: String) -> DaoResult<Game> {
-        let result = self.kv
-            .get(self.ctx.as_ref(), &key)
+    async fn get(&mut self, key: Lowercase) -> DaoResult<GameData> {
+        let result = self
+            .kv
+            .get(self.ctx.as_ref(), key.value())
             .await
             .map_err(|_| DaoError::Unknown("kv actor interface error".into()))?;
         match result.exists {
@@ -38,21 +48,30 @@ impl GameDao for WasmKeyValueDao {
                     result.value
                 ))
             }),
-            false => DaoResult::Err(DaoError::NotFound(key)),
+            false => DaoResult::Err(DaoError::NotFound(key.value().to_string())),
         }
     }
-    async fn keys(&mut self) -> DaoResult<Vec<String>> {
-        let result = self.kv
-            .list_range(self.ctx.as_ref(),ALL_GAMES_KEY.into(), 0, std::i32::MAX)
+    async fn keys(&mut self) -> DaoResult<Vec<Lowercase>> {
+        let list_range_request = ListRangeRequest {
+            list_name: ALL_GAMES_KEY.into(),
+            start: 0,
+            stop: std::i32::MAX,
+        };
+        let result = self
+            .kv
+            .list_range(self.ctx.as_ref(), &list_range_request)
             .await
             .map_err(|e| DaoError::Unknown(e.to_string()))?;
-        Ok(result.values)
+        Ok(result
+            .into_iter()
+            .map(|s| Lowercase::new(s.as_str()))
+            .collect())
     }
-    async fn set(&mut self, key: String, game: Game) -> DaoResult<()> {
+    async fn set(&mut self, key: Lowercase, game: GameData) -> DaoResult<()> {
         let set_request = SetRequest {
-            key,
+            key: key.value().to_string(),
             value: json!(game).to_string(),
-            expires: 0
+            expires: 0,
         };
         self.kv
             .set(self.ctx.as_ref(), &set_request)
@@ -60,7 +79,7 @@ impl GameDao for WasmKeyValueDao {
             .map_err(|e| DaoError::Unknown(e.to_string()))?;
         let list_add_request = ListAddRequest {
             list_name: ALL_GAMES_KEY.into(),
-            value: key.clone()
+            value: key.value().to_string(),
         };
         self.kv
             .list_add(self.ctx.as_ref(), &list_add_request)
