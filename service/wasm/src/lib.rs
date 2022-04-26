80d714e3c4a8771618a366f9161442bc8cf8e8e4
matthewtgilbride <matthewtgilbride@gmail.com>
2022-04-26 15:40:09 -0400

    fix(kv-dynamodb): ServiceError handling

--- service/wasm/src/lib.rs
+++ service/wasm/src/lib.rs
@@ -33,125 +33,12 @@ impl CodenamesActor {
         let board_generator = Box::new(BoardGeneratorWasmCloud);
         let dao = Box::new(WasmKeyValueDao::new(ctx));
         let service = GameService::new(word_generator, board_generator, dao)
-            .map_err(|e| std_to_rpc_error(e))?;
+            .map_err(|e| to_rpc_error(e))?;
 
         let &method = &req.method.as_str();
         let segments = path_segments(req);
 
-        let routing_result: Result<Value, ServiceError> = match (method, &segments[..]) {
-            // get a random game key
-            ("GET", []) => {
-                debug_route("random game").await?;
-                let json = json!(service.random_name().await.map_err(|e| to_rpc_error(e))?);
-                Ok(json)
-            }
-
-            // create a game
-            ("POST", ["game"]) => {
-                debug_route("create game").await?;
-
-                let body_str = std::str::from_utf8(req.body.as_slice())
-                     .map_err(|_| RpcError::Other("error body utf8".to_string()))?;
-
-                let body: GameNameBody = serde_json::from_str(body_str)
-                         .map_err(|e| RpcError::Other(e.to_string()))?;
-
-                let game = service
-                    .new_game(body.game_name)
-                    .await
-                    .map_err(to_rpc_error)?;
-                Ok(json!(game))
-            }
-
-            // get a list of all games
-            ("GET", ["game"]) => {
-                debug_route("get all games").await?;
-                let games = service.clone().find().await.map_err(to_rpc_error)?;
-                Ok(json!(games))
-            }
-
-            // get an existing game
-            ("GET", ["game", game_key]) => {
-                debug_route("get game").await?;
-                let game = service
-                    .clone()
-                    .get(game_key, &None, &None)
-                    .await
-                    .map_err(to_rpc_error)?;
-                Ok(json!(game))
-            }
-
-            // join a game as a player
-            ("PUT", ["game", game_key, "join"]) => {
-                debug_route("join").await?;
-                let player: Player = serde_json::from_str(
-                    std::str::from_utf8(req.body.as_slice())
-                        .map_err(|e| RpcError::Other(e.to_string()))?,
-                )
-                .map_err(|e| RpcError::Other(e.to_string()))?;
-                let updated_game = service
-                    .join(game_key.to_string(), player)
-                    .await
-                    .map_err(to_rpc_error)?;
-                Ok(json!(updated_game))
-            }
-
-            // end the current team's turn
-            ("PUT", ["game", game_key, "end-turn"]) => {
-                debug_route("end turn").await?;
-                let updated_game = service
-                    .end_turn(game_key.to_string())
-                    .await
-                    .map_err(to_rpc_error)?;
-                Ok(json!(updated_game))
-            }
-
-            // get a player's view of the game
-            ("GET", ["game", game_key, player_name_encoded]) => {
-                debug_route("get player game").await?;
-                let player_name =
-                    decode(player_name_encoded).map_err(|e| RpcError::Other(e.to_string()))?;
-                let game = service
-                    .clone()
-                    .get(game_key, &Some(player_name), &None)
-                    .await
-                    .map_err(to_rpc_error)?;
-                Ok(json!(game))
-            }
-
-            // guess a word
-            ("PUT", ["game", game_key, player_name_encoded, "guess", index]) => {
-                debug_route("guess").await?;
-                let player_name =
-                    decode(player_name_encoded).map_err(|e| RpcError::Other(e.to_string()))?;
-                let board_index_result = index.parse::<usize>();
-                match board_index_result {
-                    Ok(board_index) => {
-                        let updated_game = service
-                            .guess(game_key.to_string(), (player_name.as_str(), board_index))
-                            .await
-                            .map_err(to_rpc_error)?;
-                        Ok(json!(updated_game))
-                    }
-                    Err(e) => Err(ServiceError::BadRequest(e.to_string())),
-                }
-            }
-
-            // leave a game
-            ("PUT", ["game", game_key, player_name_encoded, "leave"]) => {
-                debug_route("leave").await?;
-                let player_name =
-                    decode(player_name_encoded).map_err(|e| RpcError::Other(e.to_string()))?;
-                let updated_game = service
-                    .clone()
-                    .leave(game_key.to_string(), player_name.as_str())
-                    .await
-                    .map_err(to_rpc_error)?;
-                Ok(json!(updated_game))
-            }
-
-            _ => Err(ServiceError::NotFound("unmatched route".to_string())),
-        };
+        let routing_result: Result<Value, ServiceError> = do_routing(service, method, segments, req.body.as_slice()).await;
 
         routing_result
             .map(|json| HttpResponse::json(json, 200))
@@ -178,15 +65,121 @@ impl HttpServer for CodenamesActor {
     }
 }
 
+async fn do_routing(service: GameService, method: &str, segments: Vec<&str>, body_slice: &[u8]) -> Result<Value, ServiceError> {
+    match (method, &segments[..]) {
+        // get a random game key
+        ("GET", []) => {
+            debug_route("random game").await?;
+            let json = json!(GameNameBody::new(service.random_name().await?));
+            Ok(json!(json))
+        }
+
+        // create a game
+        ("POST", ["game"]) => {
+            debug_route("create game").await?;
+
+            let body_str = std::str::from_utf8(body_slice)
+                .map_err(|_| ServiceError::Unknown("error body utf8".to_string()))?;
+
+            let body: GameNameBody = serde_json::from_str(body_str)
+                .map_err(|e| ServiceError::Unknown(e.to_string()))?;
+
+            let game = service
+                .new_game(body.game_name)
+                .await?;
+            Ok(json!(game))
+        }
+        //
+        // get a list of all games
+        ("GET", ["game"]) => {
+            debug_route("get all games").await?;
+            let games = service.clone().find().await?;
+            Ok(json!(games))
+        }
+        //
+        // // get an existing game
+        ("GET", ["game", game_key]) => {
+            debug_route("get game").await?;
+            let game = service
+                .clone()
+                .get(game_key, &None, &None)
+                .await?;
+            Ok(json!(game))
+        }
+        //
+        // join a game as a player
+        ("PUT", ["game", game_key, "join"]) => {
+            debug_route("join").await?;
+            let player: Player = serde_json::from_str(
+                std::str::from_utf8(body_slice)
+                    .map_err(|e| ServiceError::Unknown(e.to_string()))?,
+            )
+                .map_err(|e| ServiceError::Unknown(e.to_string()))?;
+            let updated_game = service
+                .join(game_key.to_string(), player)
+                .await?;
+            Ok(json!(updated_game))
+        }
+        //
+        // end the current team's turn
+        ("PUT", ["game", game_key, "end-turn"]) => {
+            debug_route("end turn").await?;
+            let updated_game = service
+                .end_turn(game_key.to_string())
+                .await?;
+            Ok(json!(updated_game))
+        }
+        //
+        // get a player's view of the game
+        ("GET", ["game", game_key, player_name_encoded]) => {
+            debug_route("get player game").await?;
+            let player_name =
+                decode(player_name_encoded).map_err(|e| ServiceError::Unknown(e.to_string()))?;
+            let game = service
+                .clone()
+                .get(game_key, &Some(player_name), &None)
+                .await?;
+            Ok(json!(game))
+        }
+
+        // // guess a word
+        ("PUT", ["game", game_key, player_name_encoded, "guess", index]) => {
+            debug_route("guess").await?;
+            let player_name =
+                decode(player_name_encoded).map_err(|e| ServiceError::Unknown(e.to_string()))?;
+            let board_index_result = index.parse::<usize>();
+            match board_index_result {
+                Ok(board_index) => {
+                    let updated_game = service
+                        .guess(game_key.to_string(), (player_name.as_str(), board_index))
+                        .await?;
+                    Ok(json!(updated_game))
+                }
+                Err(e) => Err(ServiceError::BadRequest(e.to_string())),
+            }
+        }
+
+        // // leave a game
+        ("PUT", ["game", game_key, player_name_encoded, "leave"]) => {
+            debug_route("leave").await?;
+            let player_name =
+                decode(player_name_encoded).map_err(|e| ServiceError::Unknown(e.to_string()))?;
+            let updated_game = service
+                .clone()
+                .leave(game_key.to_string(), player_name.as_str())
+                .await?;
+            Ok(json!(updated_game))
+        }
+
+        _ => Err(ServiceError::NotFound("unmatched route".to_string())),
+    }
+}
+
 fn to_service_error(err: RpcError) -> ServiceError {
     ServiceError::Unknown(err.to_string())
 }
 
-fn std_to_rpc_error(err: StdError) -> RpcError {
-    RpcError::Other(err.to_string())
-}
-
-fn to_rpc_error(err: ServiceError) -> RpcError {
+fn to_rpc_error(err: StdError) -> RpcError {
     RpcError::Other(err.to_string())
 }
 
@@ -198,8 +191,8 @@ pub fn path_segments(req: &HttpRequest) -> Vec<&str> {
         .collect::<Vec<_>>()
 }
 
-async fn debug_route(msg: &str) -> RpcResult<()> {
-    info!("matched route: {}", msg);
+async fn debug_route(msg: &str) -> ServiceResult<()> {
+    log_stuff(format!("matched route: {}", msg)).await?;
     Ok(())
 }
 
